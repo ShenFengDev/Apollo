4d4ec21053873b9da1f7ad5f98af12ff744a0d80
ShenFeng <wddsfgf@outlook.com>
2023-07-01 11:34:24 +0800

    修复打开背包会停止疾跑一下的bug

--- shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/InventoryMove.kt
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/InventoryMove.kt
@@ -1,108 +1,56 @@
-/*
- * LiquidBounce Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/CCBlueX/LiquidBounce/
- */
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
-import net.ccbluex.liquidbounce.api.minecraft.network.play.client.ICPacketPlayer
-import net.ccbluex.liquidbounce.event.*
+import net.ccbluex.liquidbounce.event.ClickWindowEvent
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.value.BoolValue
-import net.minecraft.client.gui.GuiChat
-import net.minecraft.client.gui.inventory.GuiContainer
-import org.lwjgl.input.Keyboard
 
 @ModuleInfo(name = "InventoryMove", description = "Allows you to walk while an inventory is opened.", category = ModuleCategory.MOVEMENT)
 class InventoryMove : Module() {
-    private val noDetectableValue = BoolValue("NoDetectable", false)
-    private val rotateValue = BoolValue("Rotate", true)
+
+    private val undetectable = BoolValue("Undetectable", false)
     val aacAdditionProValue = BoolValue("AACAdditionPro", false)
     private val noMoveClicksValue = BoolValue("NoMoveClicks", false)
 
-    private val blinkPacketList = mutableListOf<ICPacketPlayer>()
-    var lastInvOpen = false
-        private set
-    var invOpen = false
-        private set
+    private val affectedBindings = arrayOf(
+        mc.gameSettings.keyBindForward,
+        mc.gameSettings.keyBindBack,
+        mc.gameSettings.keyBindRight,
+        mc.gameSettings.keyBindLeft,
+        mc.gameSettings.keyBindJump,
+        mc.gameSettings.keyBindSprint
+    )
 
-    private fun updateKeyState() {
-        if (mc2.currentScreen != null && mc2.currentScreen !is GuiChat && (!noDetectableValue.get() || mc2.currentScreen !is GuiContainer)) {
-            mc.gameSettings.keyBindForward.pressed = mc.gameSettings.isKeyDown(mc.gameSettings.keyBindForward)
-            mc.gameSettings.keyBindBack.pressed = mc.gameSettings.isKeyDown(mc.gameSettings.keyBindBack)
-            mc.gameSettings.keyBindRight.pressed = mc.gameSettings.isKeyDown(mc.gameSettings.keyBindRight)
-            mc.gameSettings.keyBindLeft.pressed = mc.gameSettings.isKeyDown(mc.gameSettings.keyBindLeft)
-            mc.gameSettings.keyBindJump.pressed = mc.gameSettings.isKeyDown(mc.gameSettings.keyBindJump)
-            mc.gameSettings.keyBindSprint.pressed = mc.gameSettings.isKeyDown(mc.gameSettings.keyBindSprint)
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        tick()
+    }
 
-            if (rotateValue.get()) {
-                if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
-                    if (mc.thePlayer!!.rotationPitch > -90) {
-                        mc.thePlayer!!.rotationPitch -= 5
-                    }
-                }
-                if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
-                    if (mc.thePlayer!!.rotationPitch < 90) {
-                        mc.thePlayer!!.rotationPitch += 5
-                    }
-                }
-                if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-                    mc.thePlayer!!.rotationYaw -= 5
-                }
-                if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
-                    mc.thePlayer!!.rotationYaw += 5
-                }
+    fun tick() {
+        if (!classProvider.isGuiChat(mc.currentScreen) && !classProvider.isGuiIngameMenu(mc.currentScreen) && (!undetectable.get() || !classProvider.isGuiContainer(mc.currentScreen))) {
+            for (affectedBinding in affectedBindings) {
+                affectedBinding.pressed = mc.gameSettings.isKeyDown(affectedBinding)
             }
         }
     }
 
     @EventTarget
-    fun onMotion(event: MotionEvent) {
-        updateKeyState()
-    }
-
-    @EventTarget
-    fun onScreen(event: ScreenEvent) {
-        updateKeyState()
-    }
-
-    @EventTarget
     fun onClick(event: ClickWindowEvent) {
-        if (noMoveClicksValue.get() && MovementUtils.isMoving) {
+        if (noMoveClicksValue.get() && MovementUtils.isMoving)
             event.cancelEvent()
-        }
-    }
-
-    @EventTarget
-    fun onWorld(event: WorldEvent) {
-        blinkPacketList.clear()
-        invOpen = false
-        lastInvOpen = false
     }
 
     override fun onDisable() {
-        if (!mc.gameSettings.isKeyDown(mc.gameSettings.keyBindForward) || mc.currentScreen != null) {
-            mc.gameSettings.keyBindForward.pressed = false
-        }
-        if (!mc.gameSettings.isKeyDown(mc.gameSettings.keyBindBack) || mc.currentScreen != null) {
-            mc.gameSettings.keyBindBack.pressed = false
-        }
-        if (!mc.gameSettings.isKeyDown(mc.gameSettings.keyBindRight) || mc.currentScreen != null) {
-            mc.gameSettings.keyBindRight.pressed = false
-        }
-        if (!mc.gameSettings.isKeyDown(mc.gameSettings.keyBindLeft) || mc.currentScreen != null) {
-            mc.gameSettings.keyBindLeft.pressed = false
-        }
-        if (!mc.gameSettings.isKeyDown(mc.gameSettings.keyBindJump) || mc.currentScreen != null) {
-            mc.gameSettings.keyBindJump.pressed = false
-        }
-        if (!mc.gameSettings.isKeyDown(mc.gameSettings.keyBindSprint) || mc.currentScreen != null) {
-            mc.gameSettings.keyBindSprint.pressed = false
-        }
+        val isIngame = mc.currentScreen != null
 
+        for (affectedBinding in affectedBindings) {
+            if (!mc.gameSettings.isKeyDown(affectedBinding) || isIngame)
+                affectedBinding.pressed = false
+        }
     }
 
     override val tag: String?
